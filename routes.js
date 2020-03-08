8e1104c233ed5f94a4bee3bccd7a9de091c51948
Lei Shi <me@leishi.io>
2020-03-07 16:35:34 -0800

    Update routes.js

--- routes.js
+++ routes.js
@@ -3,6 +3,8 @@ const Routes = {
     google: 'https://www.google.com',
     baidu: 'https://www.baidu.com',
     'xy-problem': 'https://en.wikipedia.org/wiki/XY_problem',
+    whataboutXXX: 'https://en.wikipedia.org/wiki/Whataboutism',
+    whataboutism: 'https://en.wikipedia.org/wiki/Whataboutism'
 }
 
 module.exports = Routes;

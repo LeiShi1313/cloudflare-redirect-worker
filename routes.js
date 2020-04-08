062bb6e19b2a2f796beaf1d16c7c2f3dea0cf635
Lei Shi <me@leishi.io>
2020-04-08 14:52:26 -0700

    Update routes.js

--- routes.js
+++ routes.js
@@ -6,7 +6,10 @@ const Routes = {
     whataboutXXX: 'https://en.wikipedia.org/wiki/Whataboutism',
     whataboutism: 'https://en.wikipedia.org/wiki/Whataboutism',
     ETH: 'https://en.wikipedia.org/wiki/ETH_Zurich',
-    'how-to-ask': 'https://stackoverflow.com/help/how-to-ask'
+    'how-to-ask': 'https://stackoverflow.com/help/how-to-ask',
+    'ac-island': 'https://eugeneration.github.io/HappyIslandDesigner/',
+    'ac-qr': 'https://acpatterns.com/editor',
+    'ac-turnip': 'https://elxris.github.io/Turnip-Calculator/'
 }
 
 module.exports = Routes;

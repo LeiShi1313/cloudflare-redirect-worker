6ac40c9496b2880d292f4315b44f94a6d6524d5c
Lei Shi <me@leishi.io>
2020-07-23 14:05:30 -0700

    Update routes.js

--- routes.js
+++ routes.js
@@ -11,7 +11,9 @@ const Routes = {
     'ac-qr': 'https://acpatterns.com/editor',
     'ac-turnip': 'https://elxris.github.io/Turnip-Calculator/',
     'ssrcloud': 'https://ntt-co-jp.club/auth/register?code=AFHj',
-    'AFHj': 'https://ntt-co-jp.club/auth/register?code=AFHj'
+    'AFHj': 'https://ntt-co-jp.club/auth/register?code=AFHj',
+    '%E8%96%AF%E6%9D%A1': 'https://ntt-co-jp.club/auth/register?code=AFHj',
+    '%E5%88%AB%E7%94%A8%E6%88%88%E5%B0%94%E5%B7%B4%E4%B9%94%E5%A4%AB%E7%9A%84': 'https://ntt-co-jp.club/auth/register?code=AFHj'
 }
 
 module.exports = Routes;

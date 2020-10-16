cdfa2d46315da466e40e477b3058f7190d5ae3c4
Lei Shi <me@leishi.io>
2020-10-15 22:36:04 -0700

    Update routes.js

--- routes.js
+++ routes.js
@@ -10,10 +10,8 @@ const Routes = {
     'ac-island': 'https://eugeneration.github.io/HappyIslandDesigner/',
     'ac-qr': 'https://acpatterns.com/editor',
     'ac-turnip': 'https://elxris.github.io/Turnip-Calculator/',
-    'ssrcloud': 'https://ntt-co-jp.club/auth/register?code=AFHj',
-    'AFHj': 'https://ntt-co-jp.club/auth/register?code=AFHj',
-    '%E8%96%AF%E6%9D%A1': 'https://ntt-co-jp.club/auth/register?code=AFHj',
-    '%E5%88%AB%E7%94%A8%E6%88%88%E5%B0%94%E5%B7%B4%E4%B9%94%E5%A4%AB%E7%9A%84': 'https://ntt-co-jp.club/auth/register?code=AFHj'
+    'ssrcloud': 'https://邀请01.很有精神.com/auth/register?code=OfvL',
+    'cnix': 'https://邀请01.很有精神.com/auth/register?code=OfvL'
 }
 
 module.exports = Routes;
